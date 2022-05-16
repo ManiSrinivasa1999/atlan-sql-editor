ee9402a1fe827a5bf3f42723d8b3189c91177012
BADAM MANISRINIVASA NARAYANA <50072053+ManiSrinivasa1999@users.noreply.github.com>
2022-05-16 11:10:32 +0530

    feat: read data from csv using content

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,11 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-col cols="12">
+      <v-data-table
+        :headers="headers"
+        :items="categoriesData.body"
+        class="elevation-1"
+      ></v-data-table>
     </v-col>
   </v-row>
 </template>
@@ -83,5 +13,26 @@
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ $content, params, error }) {
+    const slug = params.slug || 'csv/categories'
+    const categoriesData = await $content(slug)
+      .fetch()
+      .catch((_err) => {
+        error({ statusCode: 404, message: 'Page not found' })
+      })
+    return {
+      categoriesData,
+    }
+  },
+  data() {
+    return {
+      headers: [
+        { text: 'Category ID', value: 'categoryID' },
+        { text: 'Category Name', value: 'categoryName' },
+        { text: 'Description', value: 'description' },
+        { text: 'Picture', value: 'picture' },
+      ],
+    }
+  },
 }
 </script>
