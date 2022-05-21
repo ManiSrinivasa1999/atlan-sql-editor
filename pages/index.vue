e2f8748b6a9341fa0987b12fa9be77b0123fe1bf
BADAM MANISRINIVASA NARAYANA <50072053+ManiSrinivasa1999@users.noreply.github.com>
2022-05-21 08:26:46 +0530

    feat: update table ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container>
-    <v-row justify="center" class="mt-2">
-      <v-col cols="12" md="2" class="text-prop pr-0"> Select * from </v-col>
-      <v-col cols="12" md="2" class="pl-0">
+    <v-row v-if="$vuetify.breakpoint.mdAndUp" justify="center" class="mt-2">
+      <v-col cols="12" xl="2" lg="3" md="4" class="text-prop pr-0">
+        Select * from
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" class="pl-0">
         <v-select
           v-model="selectedTable"
           :items="Object.keys(tablesList)"
@@ -14,13 +16,20 @@
         ></v-select>
       </v-col>
     </v-row>
-    <v-row v-if="true" align="center" justify="center">
+    <v-row v-if="$vuetify.breakpoint.mdAndUp" align="center" justify="center">
       <Table
         v-if="isSelected"
         :headers="headers"
-        :items="items.body"
+        :items="items"
         :loading="loading"
       />
+    </v-row>
+    <v-row
+      v-if="$vuetify.breakpoint.smAndDown"
+      justify="center"
+      class="mt-2 text-h4 font-weignt-bold"
+    >
+      Coming soon...
     </v-row>
   </v-container>
 </template>
@@ -58,10 +67,45 @@ export default {
       this.loading = true
       const slug = this.tablesList[this.selectedTable]
       try {
-        this.items = await this.$content(`csv/${slug}`).fetch()
+        this.items = []
+        const data = await this.$content(`csv/${slug}`).fetch()
         const jsonData = await this.$content('json/header').fetch()
         this.headers = jsonData[this.selectedTable]
-        // console.log(this.items)
+        if (this.headers[this.headers.length - 1].value === 'picture') {
+          for (let index = 0; index < data.body.length; index += 1) {
+            this.items.push({
+              categoryID: data.body[index].categoryID,
+              categoryName: data.body[index].categoryName,
+              description: data.body[index].description,
+              picture: data.body[index].picture.slice(0, 20),
+            })
+          }
+        } else if (this.headers[this.headers.length - 1].value === 'photo') {
+          for (let index = 0; index < data.body.length; index += 1) {
+            this.items.push({
+              employeeID: data.body[index].employeeID,
+              lastName: data.body[index].lastName,
+              firstName: data.body[index].firstName,
+              title: data.body[index].title,
+              titleOfCourtesy: data.body[index].titleOfCourtesy,
+              birthDate: data.body[index].birthDate,
+              hireDate: data.body[index].hireDate,
+              address: data.body[index].address,
+              city: data.body[index].city,
+              region: data.body[index].region,
+              postalCode: data.body[index].postalCode,
+              country: data.body[index].country,
+              homePhone: data.body[index].homePhone,
+              extension: data.body[index].extension,
+              notes: data.body[index].notes.slice(0, 20),
+              reportsTo: data.body[index].reportsTo,
+              photoPath: data.body[index].photoPath.slice(0, 10),
+              photo: data.body[index].photo.slice(0, 10),
+            })
+          }
+        } else {
+          this.items = data.body
+        }
       } catch (error) {
         error({ statusCode: 404, message: 'Page not found' })
       } finally {
@@ -69,6 +113,67 @@ export default {
         this.loading = false
       }
     },
+    // async innerJoin() {
+    //   this.loading = true
+    //   const results = []
+    //   const tableOne = await this.$content(`csv/employee_territories`).fetch()
+    //   const tableTwo = await this.$content(`csv/employees`).fetch()
+    //   // const jsonData = await this.$content('json/header').fetch()
+    //   // debugger
+    //   const tableOneBody = tableOne.body
+    //   const tableTwoBody = tableTwo.body
+    //   this.headers = [
+    //     { text: 'Employee ID', value: 'employeeID' },
+    //     { text: 'Last name', value: 'lastName' },
+    //     { text: 'First name', value: 'firstName' },
+    //     { text: 'Title', value: 'title' },
+    //     { text: 'Title of courtesy', value: 'titleOfCourtesy' },
+    //     { text: 'Birth date', value: 'birthDate' },
+    //     { text: 'Hire date', value: 'hireDate' },
+    //     { text: 'Address', value: 'address' },
+    //     { text: 'City', value: 'city' },
+    //     { text: 'Region', value: 'region' },
+    //     { text: 'Postal code', value: 'postalCode' },
+    //     { text: 'Country', value: 'country' },
+    //     { text: 'Home phone', value: 'homePhone' },
+    //     { text: 'Extension', value: 'extension' },
+    //     { text: 'Photo', value: 'photo' },
+    //     { text: 'Notes', value: 'notes' },
+    //     { text: 'Reports to', value: 'reportsTo' },
+    //     { text: 'Photo path', value: 'photoPath' },
+    //     { text: 'Territory Name', value: 'territoryID' },
+    //   ]
+    //   for (let i = 0; i < tableOneBody.length; i++) {
+    //     for (let j = 0; j < tableTwoBody.length; j++) {
+    //       if (tableOneBody[i].employeeID === tableTwoBody[j].employeeID) {
+    //         results.push({
+    //           employeeID: tableTwoBody[i].employeeID,
+    //           lastName: tableTwoBody[i].lastName,
+    //           firstName: tableTwoBody[i].firstName,
+    //           title: tableTwoBody[i].title,
+    //           titleOfCourtesy: tableTwoBody[i].titleOfCourtesy,
+    //           birthDate: tableTwoBody[i].birthDate,
+    //           hireDate: tableTwoBody[i].hireDate,
+    //           address: tableTwoBody[i].address,
+    //           city: tableTwoBody[i].city,
+    //           region: tableTwoBody[i].region,
+    //           postalCode: tableTwoBody[i].postalCode,
+    //           country: tableTwoBody[i].country,
+    //           homePhone: tableTwoBody[i].homePhone,
+    //           extension: tableTwoBody[i].extension,
+    //           photo: tableTwoBody[i].photo,
+    //           notes: tableTwoBody[i].notes,
+    //           reportsTo: tableTwoBody[i].reportsTo,
+    //           photoPath: tableTwoBody[i].photoPath,
+    //           territoryID: tableOneBody[i].territoryID,
+    //         })
+    //       }
+    //     }
+    //   }
+    //   this.isSelected = true
+    //   this.loading = false
+    //   this.items = results
+    // },
   },
 }
 </script>
